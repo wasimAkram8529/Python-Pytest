50c7cefe6f72bcae662962dd263604f895768b9a
wasimAkram8529 <codelikearockstar@gamil.com>
2025-10-25 04:35:24 +0000

    Skip test commented

--- tests/test_self01.py
+++ tests/test_self01.py
@@ -5,8 +5,8 @@ import sys
 def test_example():
     assert 1 == 2
 
-@pytest.mark.skipif(sys.platform != 'win32', reason = "Skip this test on window os")
-def test_skip_with_condition():
-    assert 1 == 2
+# @pytest.mark.skipif(sys.platform != 'win32', reason = "Skip this test on window os")
+# def test_skip_with_condition():
+#     assert 1 == 2
 
 
